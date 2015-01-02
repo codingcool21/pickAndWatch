3433f0b59b26b4d5cc9f4b1f904dd6d79d69c6f1
codingcool21 <bryan.quadras2001@gmail.com>
2015-01-02 18:57:29 +0400

    All functionality of this app is now working. Release 1.0

--- Apple/ios.js
+++ Apple/ios.js
@@ -7,8 +7,12 @@ general.firebaseRoot = new Firebase("https://pickandwatch.firebaseio.com/");
 // Create a child reference to the matchnum area
 general.firebaseMatchNumRef = general.firebaseRoot.child("matchnum");
 
-general.sendVideoID = function () {
-    var v = $("#input_box").val();
+general.sendVideoID = function (value) {
+    if (value) {
+        var v = value;
+    } else {
+        var v = $("#input_box").val();
+    }
     var o = {};
     o["video_id"] = v;
     o["play"] = false;
@@ -38,6 +42,24 @@ general.loadPage = function (page) {
         break;
     }
 }
+general.centerElementOnPage = function (element, element_value, css_size_word, top_or_left) {
+    //alert(element + " " + element_value + " " + css_size_word + " "+ top_or_left);
+    var windowmeasure;
+    if (css_size_word == "em") {
+        var css_word = "em";
+    }
+    if (css_size_word == "px") {
+        var css_word = "px";
+    }
+    if (top_or_left == "top") {
+        windowmeasure = $(window).innerHeight()
+    } else {
+        windowmeasure = $(window).innerWidth()
+    }
+    //alert(windowmeasure);
+    $(element).css("position", "relative");
+    $(element).css(top_or_left, windowmeasure / 2 - (element_value * 0.5) + css_word);
+}
 $(function () {
     // Set up an enter handler for the textbox
     // When the enter button is pressed, set the
@@ -49,4 +71,8 @@ $(function () {
             general.sendVideoID();
         }
     });
+});
+
+$(window).resize(function () {
+    general.centerElementOnPage("#result_view", $("#result_view").innerWidth(), "px", "left");
 });
--- PC/pc.js
+++ PC/pc.js
@@ -12,11 +12,38 @@ general.playFirebase.on("value", function (v) {
         var video_url = "http://www.youtube.com/watch?v=" + general.videoid;
         jwplayer("video_firebase").setup({
             file: video_url,
-            width: 800,
-            height: 600
+            width: $(window).innerWidth(),
+            height: $(window).innerHeight()
+        });
+        jwplayer("video_firebase").onReady(function () {
+            jwplayer("video_firebase").play(true);
         });
     }
 });
+general.centerElementOnPage = function (element, element_value, css_size_word, top_or_left) {
+    //alert(element + " " + element_value + " " + css_size_word + " "+ top_or_left);
+    var windowmeasure;
+    if (css_size_word == "em") {
+        var css_word = "em";
+    }
+    if (css_size_word == "px") {
+        var css_word = "px";
+    }
+    if (top_or_left == "top") {
+        windowmeasure = $(window).innerHeight()
+    } else {
+        windowmeasure = $(window).innerWidth()
+    }
+    //alert(windowmeasure);
+    $(element).css("position", "relative");
+    $(element).css(top_or_left, windowmeasure / 2 - (element_value * 0.5) + css_word);
+}
 $(function () {
-
+    general.centerElementOnPage("#video_firebase", $("#video_firebase").innerWidth(), "px", "left");
+    general.centerElementOnPage("#video_firebase", $("#video_firebase").innerHeight(), "px", "top");
+    $(window).resize(function () {
+        jwplayer().resize($(window).innerWidth(), $(window).innerHeight());
+        general.centerElementOnPage("#video_firebase", $("#video_firebase").innerWidth(), "px", "left");
+        general.centerElementOnPage("#video_firebase", $("#video_firebase").innerHeight(), "px", "top");
+    });
 });
